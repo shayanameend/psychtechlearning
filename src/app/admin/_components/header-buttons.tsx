3cee359c09ae3804affd530c5c78b47cdc3408c4
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-02-21 03:36:47 +0500

    feat: replace SignOutButton with HeaderButtons component in admin and dashboard layouts

---
+++ src/app/admin/_components/header-buttons.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { EyeIcon, LogOutIcon } from "lucide-react";
+import { default as Link } from "next/link";
+
+import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+import { useUserContext } from "~/providers/user-provider";
+import { paths } from "~/routes/paths";
+
+export function HeaderButtons() {
+  const { user, setToken, setUser, setIsLoading } = useUserContext();
+
+  return (
+    <div className={cn("flex gap-4 items-center")}>
+      {user && user.role === "ADMIN" && (
+        <Button variant="outline" size="icon" className={cn("size-6")}>
+          <Link href={paths.app.dashboard.root()}>
+            <EyeIcon className={cn("size-4")} />
+          </Link>
+        </Button>
+      )}
+      <Button
+        onClick={() => {
+          localStorage.removeItem("token");
+
+          setToken?.(null);
+          setUser?.(null);
+          setIsLoading?.(false);
+        }}
+        variant="outline"
+        size="icon"
+        className="size-6 [&_svg]:size-3"
+      >
+        <LogOutIcon />
+      </Button>
+    </div>
+  );
+}
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,12 @@
 import type { PropsWithChildren } from "react";
 
-import { GalleryVerticalEndIcon, LogOutIcon } from "lucide-react";
+import { EyeIcon, GalleryVerticalEndIcon, LogOutIcon } from "lucide-react";
 import { default as Link } from "next/link";
 
+import { HeaderButtons } from "~/app/admin/_components/header-buttons";
+import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 import { paths } from "~/routes/paths";
-import { SignOutButton } from "./_components/sign-out-button";
 
 export default function DashboardLayout({
   children,
@@ -15,22 +16,24 @@ export default function DashboardLayout({
       <header
         className={cn("p-6 md:p-10 flex gap-4 items-center justify-between")}
       >
-        <Link
-          href={paths.app.dashboard.root()}
-          className={cn("flex justify-center md:justify-start")}
-        >
-          <h1 className={cn("flex items-center gap-2 font-medium")}>
-            <span
-              className={cn(
-                "flex items-center justify-center rounded-md size-6 bg-primary text-primary-foreground",
-              )}
-            >
-              <GalleryVerticalEndIcon className={cn("size-4")} />
-            </span>
-            <span>Psychtech Learning</span>
-          </h1>
-        </Link>
-        <SignOutButton />
+        <div>
+          <Link
+            href={paths.app.dashboard.root()}
+            className={cn("flex justify-center md:justify-start")}
+          >
+            <h1 className={cn("flex items-center gap-2 font-medium")}>
+              <span
+                className={cn(
+                  "flex items-center justify-center rounded-md size-6 bg-primary text-primary-foreground",
+                )}
+              >
+                <GalleryVerticalEndIcon className={cn("size-4")} />
+              </span>
+              <span>Psychtech Learning</span>
+            </h1>
+          </Link>
+        </div>
+        <HeaderButtons />
       </header>
       <main>{children}</main>
     </>
