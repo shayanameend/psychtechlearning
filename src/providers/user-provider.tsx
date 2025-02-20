c174d037747635f969fb9fd86d268c83c39277b0
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-02-20 06:54:08 +0500

    feat: remove console log from user context refresh function

--- src/providers/user-provider.tsx
+++ src/providers/user-provider.tsx
@@ -47,8 +47,6 @@ export const useUserContext = () => {
 async function refresh() {
   const token = localStorage.getItem("token");
 
-  console.log(token);
-
   const response = await axios.post(
     paths.api.auth.refresh(),
     {},
