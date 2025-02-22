6883cca4e50a5b465312d3f870bafbd624590ee6
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-02-22 08:17:59 +0500

    feat: add CourseNotes component for managing user notes in the admin dashboard

---
+++ src/app/dashboard/_components/course-section-notes.tsx
@@ -0,0 +1,181 @@
+"use client";
+
+import { EditIcon, PlusIcon, Trash2Icon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { Button } from "~/components/ui/button";
+import { Textarea } from "~/components/ui/textarea";
+import { cn } from "~/lib/utils";
+
+interface Flashcard {
+  id: string;
+  question: string;
+  answer: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface TestQuestion {
+  id: string;
+  question: string;
+  answers: string[];
+  correctAnswer: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface SectionUserNote {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Section {
+  id: string;
+  sectionOrder: number;
+  sectionTitle: string;
+  sectionDescription: string;
+  guideLabel: string;
+  guideLink: string;
+  flashcardsLabel: string;
+  sampleTestLabel: string;
+  finalTestLabel: string;
+  flashcards: Flashcard[];
+  sampleTestQuestions: TestQuestion[];
+  finalTestQuestions: TestQuestion[];
+  sectionUserNotes: SectionUserNote[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export function CourseNotes({
+  sectionUserNotes,
+  showNotes,
+}: Readonly<{ sectionUserNotes: SectionUserNote[]; showNotes: boolean }>) {
+  const [notes, setNotes] = useState<SectionUserNote[]>(sectionUserNotes);
+  const [isEditing, setIsEditing] = useState(-1);
+  const [noteValue, setNoteValue] = useState("");
+
+  useEffect(() => {
+    setNoteValue(notes[isEditing]?.content || "");
+  }, [isEditing, notes[isEditing]?.content]);
+
+  return (
+    <div
+      className={cn(
+        "space-y-3 lg:space-y-6",
+        !showNotes && "w-1/3 hidden lg:block",
+      )}
+    >
+      <article>
+        <div className={cn("flex justify-between items-center")}>
+          <h3 className={cn("text-primary text-lg font-medium")}>Your Notes</h3>
+          <Button
+            onClick={() => {
+              if (notes[notes.length - 1]?.content.trim() !== "") {
+                setNotes((prev) => [
+                  ...prev,
+                  {
+                    id: String(new Date().getTime()),
+                    content: "",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                  },
+                ]);
+
+                setIsEditing(notes.length || 0);
+              }
+            }}
+            variant="outline"
+            size="icon"
+            className={cn("size-5 rounded-sm")}
+          >
+            <PlusIcon />
+          </Button>
+        </div>
+        <p className={cn("text-gray-600 text-sm")}>
+          Here you can find the notes you've taken while studying this section.
+          These notes are personal to you and can help reinforce your learning.
+          Feel free to add, edit, or delete any notes as you progress through
+          the material.
+        </p>
+      </article>
+      <article>
+        {notes.length < 1 ? (
+          <p className={cn("text-gray-600 text-sm")}>
+            No notes added by users.
+          </p>
+        ) : (
+          <ul className={cn("space-y-2 pl-3")}>
+            {notes.map((note, index) => (
+              <li
+                key={note.id}
+                className={cn("text-gray-600 text-sm list-disc")}
+              >
+                {isEditing === index ? (
+                  <Textarea
+                    value={noteValue}
+                    onChange={(event) => {
+                      setNoteValue(event.currentTarget.value);
+                    }}
+                    onKeyDown={(event) => {
+                      if (event.key === "Enter") {
+                        setIsEditing(-1);
+                        if (event.currentTarget.value.trim() === "") {
+                          setNotes((prev) =>
+                            prev ? prev.filter((n) => n.id !== note.id) : prev,
+                          );
+                        } else {
+                          setNotes((prev) =>
+                            prev.map((n) => {
+                              if (n.id === note.id) {
+                                return {
+                                  ...n,
+                                  content: noteValue,
+                                };
+                              }
+
+                              return n;
+                            }),
+                          );
+                        }
+                      }
+                    }}
+                    className={cn("resize-none")}
+                  />
+                ) : (
+                  <>
+                    <span>{note.content}</span>
+                    <Button
+                      onClick={() => {
+                        setIsEditing(index);
+                      }}
+                      variant="link"
+                      size="icon"
+                      className={cn("ml-1 size-6")}
+                    >
+                      <EditIcon />
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        setNotes((prev) =>
+                          prev ? prev.filter((n) => n.id !== note.id) : prev,
+                        );
+                      }}
+                      variant="link"
+                      size="icon"
+                      className={cn("ml-1 size-6")}
+                    >
+                      <Trash2Icon className={cn("text-red-500")} />
+                    </Button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </article>
+    </div>
+  );
+}
---
+++ src/app/dashboard/_components/course-section.tsx
@@ -0,0 +1,406 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Label } from "~/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { cn } from "~/lib/utils";
+import { CourseNotes } from "./course-section-notes";
+
+interface Flashcard {
+  id: string;
+  question: string;
+  answer: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface TestQuestion {
+  id: string;
+  question: string;
+  answers: string[];
+  correctAnswer: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface SectionUserNote {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Section {
+  id: string;
+  sectionOrder: number;
+  sectionTitle: string;
+  sectionDescription: string;
+  guideLabel: string;
+  guideLink: string;
+  flashcardsLabel: string;
+  sampleTestLabel: string;
+  finalTestLabel: string;
+  flashcards: Flashcard[];
+  sampleTestQuestions: TestQuestion[];
+  finalTestQuestions: TestQuestion[];
+  sectionUserNotes: SectionUserNote[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export function CourseSection({
+  section,
+  showNotes,
+}: Readonly<{ section: Section; showNotes: boolean }>) {
+  const [flashcards, setFlashcards] = useState<Flashcard[]>(section.flashcards);
+  const [showFlashcard, setShowFlashcard] = useState(false);
+  const [sampleTestQuestions, setSampleTestQuestions] = useState<
+    TestQuestion[]
+  >([]);
+  const [sampleTestAnswers, setSampleTestAnswers] = useState<
+    Array<string | null>
+  >([]);
+  const [finalTestQuestions, setFinalTestQuestions] = useState<TestQuestion[]>(
+    [],
+  );
+  const [finalTestAnswers, setFinalTestAnswers] = useState<
+    Array<string | null>
+  >([]);
+
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  useEffect(() => {
+    setSampleTestAnswers(
+      Array.from({ length: sampleTestQuestions.length }, () => null),
+    );
+  }, [sampleTestQuestions]);
+
+  useEffect(() => {
+    setFinalTestAnswers(
+      Array.from({ length: finalTestQuestions.length }, () => null),
+    );
+  }, [finalTestQuestions]);
+
+  return (
+    <section className={cn("flex-1 flex")}>
+      <div
+        className={cn(
+          "w-full lg:w-2/3 space-y-4 lg:space-y-8",
+          showNotes && "hidden",
+        )}
+      >
+        <article className={cn("space-y-2")}>
+          <h3 className={cn("text-primary text-xl font-bold")}>
+            {section.sectionTitle}
+          </h3>
+          <p className={cn("text-gray-600 text-sm")}>
+            {section.sectionDescription}
+          </p>
+        </article>
+        <article className={cn("space-y-2")}>
+          <h3 className={cn("text-foreground/70 text-lg font-medium")}>
+            Study Guide
+          </h3>
+          <p className={cn("text-gray-600 text-sm")}>{section.guideLabel}</p>
+          <Button
+            onClick={() => {
+              window.open(section.guideLink, "_blank");
+            }}
+            size="sm"
+          >
+            Download
+          </Button>
+        </article>
+        <article className={cn("space-y-2")}>
+          <h3 className={cn("text-foreground/70 text-lg font-medium")}>
+            Flashcards
+          </h3>
+          <p className={cn("text-gray-600 text-sm")}>
+            {section.flashcardsLabel}
+          </p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                onClick={() => {
+                  setQuestionIndex(0);
+                }}
+                size="sm"
+              >
+                View Flashcards
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[320px] lg:max-w-[512px]">
+              <DialogHeader>
+                <DialogTitle>Flashcards: {section.sectionTitle}</DialogTitle>
+                <DialogDescription>
+                  This is a set of flashcards to help you reinforce your
+                  learning on {section.sectionTitle}.
+                </DialogDescription>
+              </DialogHeader>
+              <article className={cn("space-y-2")}>
+                <p className={cn("text-gray-600 text-sm")}>
+                  <span className={cn("mr-1")}>{questionIndex + 1}.</span>
+                  {flashcards[questionIndex]?.question}
+                  {showFlashcard && (
+                    <span className={cn("ml-1 text-primary")}>
+                      {flashcards[questionIndex]?.answer}
+                    </span>
+                  )}
+                </p>
+              </article>
+              <DialogFooter>
+                <Button
+                  onClick={() => {
+                    setShowFlashcard(!showFlashcard);
+                  }}
+                  size="sm"
+                  variant="outline"
+                  className={cn("mr-auto")}
+                >
+                  {showFlashcard ? "Hide" : "Show"}
+                </Button>
+                <Button
+                  onClick={() => {
+                    if (questionIndex > 0) {
+                      setQuestionIndex(questionIndex - 1);
+                    }
+                    setShowFlashcard(false);
+                  }}
+                  disabled={questionIndex === 0}
+                  size="sm"
+                  variant="outline"
+                >
+                  Previous
+                </Button>
+                <Button
+                  onClick={() => {
+                    if (questionIndex < sampleTestQuestions.length - 1) {
+                      setQuestionIndex(questionIndex + 1);
+                    }
+                    setShowFlashcard(false);
+                  }}
+                  size="sm"
+                  variant="outline"
+                >
+                  {questionIndex === sampleTestQuestions.length - 1
+                    ? "Submit"
+                    : "Next"}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </article>
+        <article className={cn("space-y-2")}>
+          <h3 className={cn("text-foreground/70 text-lg font-medium")}>
+            Sample Questions
+          </h3>
+          <p className={cn("text-gray-600 text-sm")}>
+            {section.sampleTestLabel}
+          </p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                onClick={() => {
+                  setQuestionIndex(0);
+                }}
+                size="sm"
+              >
+                View Questions
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[320px] lg:max-w-[512px]">
+              <DialogHeader>
+                <DialogTitle>
+                  Sample Questions: {section.sectionTitle}
+                </DialogTitle>
+                <DialogDescription>
+                  This is a set of sample questions to help you practice and
+                  reinforce your knowledge on {section.sectionTitle}. The
+                  questions consist of multiple choice questions.
+                </DialogDescription>
+              </DialogHeader>
+              <article className={cn("space-y-3")}>
+                <div className={cn("space-y-1")}>
+                  <h4 className={cn("text-lg font-medium")}>
+                    Question {questionIndex + 1}
+                  </h4>
+                  <p className={cn("text-gray-600 text-sm")}>
+                    {sampleTestQuestions[questionIndex]?.question}
+                  </p>
+                </div>
+                <RadioGroup
+                  onValueChange={(value) => {
+                    setSampleTestAnswers((prev) => {
+                      const answers = [...prev];
+
+                      answers[questionIndex] = value;
+
+                      return answers;
+                    });
+                  }}
+                >
+                  {sampleTestQuestions[questionIndex]?.answers.map(
+                    (option, index) => {
+                      return (
+                        <div
+                          // biome-ignore lint/suspicious/noArrayIndexKey: <>
+                          key={index}
+                          className="flex items-center space-x-2"
+                        >
+                          <RadioGroupItem
+                            checked={
+                              sampleTestAnswers[questionIndex] === option
+                            }
+                            value={option}
+                            id={option}
+                          />
+                          <Label htmlFor={option}>{option}</Label>
+                        </div>
+                      );
+                    },
+                  )}
+                </RadioGroup>
+              </article>
+              <DialogFooter>
+                <Button
+                  onClick={() => {
+                    if (questionIndex > 0) {
+                      setQuestionIndex(questionIndex - 1);
+                    }
+                  }}
+                  disabled={questionIndex === 0}
+                  size="sm"
+                  variant="outline"
+                >
+                  Previous
+                </Button>
+                <Button
+                  onClick={() => {
+                    if (questionIndex < sampleTestQuestions.length - 1) {
+                      setQuestionIndex(questionIndex + 1);
+                    }
+                  }}
+                  size="sm"
+                  variant="outline"
+                >
+                  {questionIndex === sampleTestQuestions.length - 1
+                    ? "Submit"
+                    : "Next"}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </article>
+        <article className={cn("space-y-2")}>
+          <h3 className={cn("text-foreground/70 text-lg font-medium")}>Test</h3>
+          <p className={cn("text-gray-600 text-sm")}>
+            {section.finalTestLabel}
+          </p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                onClick={() => {
+                  setQuestionIndex(0);
+                }}
+                size="sm"
+              >
+                Take Test
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[320px] lg:max-w-[512px]">
+              <DialogHeader>
+                <DialogTitle>Test: {section.sectionTitle}</DialogTitle>
+                <DialogDescription>
+                  This is a test to assess your knowledge on{" "}
+                  {section.sectionTitle}. The test consists of multiple choice
+                  questions and is timed. Good luck!
+                </DialogDescription>
+              </DialogHeader>
+              <article className={cn("space-y-3")}>
+                <div className={cn("space-y-1")}>
+                  <h4 className={cn("text-lg font-medium")}>
+                    Question {questionIndex + 1}
+                  </h4>
+                  <p className={cn("text-gray-600 text-sm")}>
+                    {finalTestQuestions[questionIndex]?.question}
+                  </p>
+                </div>
+                <RadioGroup
+                  onValueChange={(value) => {
+                    setFinalTestAnswers((prev) => {
+                      const answers = [...prev];
+
+                      answers[questionIndex] = value;
+
+                      return answers;
+                    });
+                  }}
+                >
+                  {finalTestQuestions[questionIndex]?.answers.map(
+                    (option, index) => {
+                      return (
+                        <div
+                          // biome-ignore lint/suspicious/noArrayIndexKey: <>
+                          key={index}
+                          className="flex items-center space-x-2"
+                        >
+                          <RadioGroupItem
+                            checked={finalTestAnswers[questionIndex] === option}
+                            value={option}
+                            id={option}
+                          />
+                          <Label htmlFor={option}>{option}</Label>
+                        </div>
+                      );
+                    },
+                  )}
+                </RadioGroup>
+              </article>
+              <DialogFooter>
+                <Button
+                  onClick={() => {
+                    if (questionIndex > 0) {
+                      setQuestionIndex(questionIndex - 1);
+                    }
+                  }}
+                  disabled={questionIndex === 0}
+                  size="sm"
+                  variant="outline"
+                >
+                  Previous
+                </Button>
+                <Button
+                  onClick={() => {
+                    if (questionIndex < finalTestQuestions.length - 1) {
+                      setQuestionIndex(questionIndex + 1);
+                    }
+                  }}
+                  size="sm"
+                  variant="outline"
+                >
+                  {questionIndex === finalTestQuestions.length - 1
+                    ? "Submit"
+                    : "Next"}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </article>
+      </div>
+      <CourseNotes
+        sectionUserNotes={section.sectionUserNotes}
+        showNotes={showNotes}
+      />
+    </section>
+  );
+}
