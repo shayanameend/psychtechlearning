39dba047669360ef59c0d7902f12b8376410d443
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-02-28 14:44:46 +0500

    feat: integrate user context into UpdateProfileForm for pre-filled profile data

--- src/app/auth/profile/update/page.tsx
+++ src/app/auth/profile/update/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { UpdateProfileForm } from "~/components/forms/update-profile-form";
+import { useUserContext } from "~/providers/user-provider";
 
 export default function UpdateProfilePage() {
+  const { user } = useUserContext();
+
   return (
     <>
-      <UpdateProfileForm />
+      <UpdateProfileForm
+        profile={{
+          firstName: user?.profile.firstName,
+          lastName: user?.profile.lastName,
+          isStudent: user?.profile.isStudent,
+          notify: user?.profile.notify,
+        }}
+      />
     </>
   );
 }
